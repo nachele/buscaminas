761fc4a9fc6b0c9301dd68c055655bd41dc235f6
nachele <59255243+nachele@users.noreply.github.com>
2025-04-08 20:35:10 +0200

    Add files via upload

--- buscaminas.py
+++ buscaminas.py
@@ -16,6 +16,9 @@ imagenTrans = pygame.transform.scale(imagen,(50,50))
 mousePos = pygame.mouse.get_pos()
 teclaMouse = pygame.mouse.get_pressed()
 bombita = 0
+fuente = pygame.font.SysFont("Arial", 20)
+color_texto = (0,0,225)
+texto = fuente.render(str(3),True, color_texto)
 
 class Casilla:
     def __init__(self,x,y,imagen):
@@ -24,7 +27,8 @@ class Casilla:
         self.bomba = False
         self.exploto = False
         self.imagen =pygame.transform.scale(pygame.image.load(imagen),(anchoCelda,altoCelda)) 
-        
+        self.descubierto = False
+        self.bombasAlrededor = 0
     def pintar(self):
         pantalla.blit(self.imagen, (self.x, self.y))
 
@@ -58,9 +62,10 @@ def casillaBomba():
 
 #VOY POR AQUI
 array0Bombas = []
+arrayBombas = []
 #array0Bombas = [[None for i in range(filas)] for x in range(columnas)]
 def casillaSinBomba():
-    global campo, anchoCelda, altoCelda, mousePos,teclaMouse,array0Bombas,bombita
+    global campo, anchoCelda, altoCelda, mousePos,teclaMouse,array0Bombas,bombita,texto, fuente, color_texto
     x = int((mousePos[0]/ anchoCelda))
     y = int((mousePos[1] / altoCelda))
     if teclaMouse[0] and x >= 0 and x <= 7 and y >= 0 and y <= 7:
@@ -79,6 +84,7 @@ def casillaSinBomba():
                         for X in range(3):
                             if((int(bomba0.y / altoCelda) - 1) + i) >= 0 and ((int(bomba0.y / altoCelda) - 1) + i) <= 7 and ((int(bomba0.x / altoCelda) - 1) + X) >= 0 and ((int(bomba0.x / altoCelda) - 1) + X) <= 7: 
                                 if campo[((int(bomba0.y / altoCelda) - 1) + i)][((int(bomba0.x / altoCelda) - 1) + X)].bomba == False:
+                                    campo[((int(bomba0.y / altoCelda) - 1) + i)][((int(bomba0.x / altoCelda) - 1) + X)].descubierto = True
                                     campo[((int(bomba0.y / altoCelda) - 1) + i)][((int(bomba0.x / altoCelda) - 1) + X)].imagen = pygame.transform.scale(pygame.image.load("imgBusca/3.png"),(anchoCelda,altoCelda))
                                 else:
                                     bombita += 1
@@ -87,13 +93,17 @@ def casillaSinBomba():
                             
     
 
+def casillasAlaoBombas():
+    for i in range(8):
+        for x in range(8):
+            print(campo[i][x].bombasAlrededor)
+            if campo[i][x].bomba ==  False:
+                for z in range(3):
+                    for y in range(3):
+                        if((i - 1)+z) >= 0 and ((i - 1) + z)  <= 7 and ((x - 1) + y) >= 0 and ((x - 1) + y) <= 7:
+                            if campo[(i - 1) + z][(x - 1) + y].bomba == True:
+                                campo[i][x].bombasAlrededor += 1
 
-                    
-
-
-
-            
-    
 
 
 
@@ -103,18 +113,22 @@ def casillaSinBomba():
 
 crearCampo()
 crearBombas()
+casillasAlaoBombas()
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     casillaBomba()
-    casillaSinBomba()
+   
     #print(len(array0Bombas))
     pantalla.fill("white") 
+    casillaSinBomba()
     for i in range(8):
         for x in range(8):
             campo[i][x].pintar()
-
+            if campo[i][x].bombasAlrededor > 0 and campo[i][x].descubierto == True:
+                pantalla.blit(fuente.render(str(campo[i][x].bombasAlrededor), True, color_texto),(x * 50 + 15, i * 50 + 15))
     pygame.display.flip()
     pygame.display.update()
 pygame.quit()
